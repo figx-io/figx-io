bce956ab581ee9a58b62f9a974e0a5a44a9b17cb
martinrossil-acies <mro@acies.dk>
2024-11-23 20:16:12 +0100

    feat: test NPM publish

--- packages/core/src/Component.ts
+++ packages/core/src/Component.ts
@@ -3,7 +3,7 @@ import type IComponent from './IComponent';
 export default class Component extends HTMLElement implements IComponent {
 	public constructor() {
 		super();
-		console.log('test NPM publish 6');
+		console.log('test NPM publish 7');
 	}
 
 	public set width(value: number | 'fill' | 'hug') {
