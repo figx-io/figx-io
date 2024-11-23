8a8a964475ea3b02a9c47941564d12abcc83f6f8
martinrossil-acies <mro@acies.dk>
2024-11-23 15:58:40 +0100

    style: enable perfectionist rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,14 @@
 import antfu from '@antfu/eslint-config';
 
 export default antfu({
+	rules: {
+		'no-console': 'warn',
+		'perfectionist/sort-array-includes': 'error',
+		'perfectionist/sort-classes': 'error',
+		'perfectionist/sort-imports': 'error',
+		'perfectionist/sort-interfaces': 'error',
+		'perfectionist/sort-objects': 'error',
+	},
 	stylistic: {
 		indent: 'tab',
 		semi: true,
