06d79f9d053ff99dc73f52c8b5143a2df5b04336
martinrossil-acies <mro@acies.dk>
2024-11-23 22:52:31 +0100

    fix: test NPM publish

--- packages/ui/src/Button.ts
+++ packages/ui/src/Button.ts
@@ -3,6 +3,7 @@ import Container from '@figx-io/core/container';
 export default class Button extends Container {
 	public constructor() {
 		super();
+		console.log('Button');
 	}
 }
 customElements.define('fx-button', Button);
