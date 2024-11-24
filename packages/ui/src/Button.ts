6150a02a41b97f7b98a7c8cd4822b812fdc0e8fd
martinrossil-acies <mro@acies.dk>
2024-11-24 17:21:22 +0100

    feat: create IButton Interface

--- packages/ui/src/Button.ts
+++ packages/ui/src/Button.ts
@@ -1,9 +1,10 @@
+import type IButton from './IButton';
 import Container from '@figx-io/core/Container';
 
-export default class Button extends Container {
+export default class Button extends Container implements IButton {
 	public constructor() {
 		super();
-		console.log('Button 2');
+		console.log('Button()');
 	}
 }
 customElements.define('fx-button', Button);
---
+++ packages/ui/src/IButton.ts
@@ -0,0 +1,5 @@
+import type IContainer from '@figx-io/core/IContainer';
+
+export default interface IButton extends IContainer {
+
+}
