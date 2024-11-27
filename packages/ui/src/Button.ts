571513c28ae885aeb192bc3422e6c9f72894a39c
martinrossil-acies <mro@acies.dk>
2024-11-27 13:42:24 +0100

    feat: bump Button version

--- packages/ui/src/Button.ts
+++ packages/ui/src/Button.ts
@@ -4,7 +4,7 @@ import Container from '@figx-io/core/Container';
 export default class Button extends Container implements IButton {
 	public constructor() {
 		super();
-		console.log('Button()');
+		console.log('Button() 2');
 	}
 }
 customElements.define('fx-button', Button);
