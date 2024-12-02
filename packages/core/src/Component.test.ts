a80938841da743c99b811bc692fcdb60f1ac6b13
martinrossil-acies <mro@acies.dk>
2024-12-02 22:37:52 +0100

    chore: rename test

--- packages/core/src/Component.test.ts
+++ packages/core/src/Component.test.ts
@@ -2,8 +2,14 @@ import type IComponent from './IComponent';
 import { describe, expect, it } from 'vitest';
 import Component from './Component';
 
-describe('icomponent', (): void => {
-	describe('properties', (): void => {
+describe('component', () => {
+	describe('default style properties', () => {
+		it('default style.display should be "inline-flex"', () => {
+			const component = new Component();
+			expect(component.style.display).toBe('inline-flex');
+		});
+	});
+	describe('default properties', () => {
 		describe('width', (): void => {
 			it('default width should be Number.NaN', (): void => {
 				const component: IComponent = new Component();
