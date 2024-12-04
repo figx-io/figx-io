a2c818154c2094aa68e1f4fbdd9b91f99b4859b7
martinrossil-acies <mro@acies.dk>
2024-12-04 08:45:31 +0100

    test: create size assertion

--- packages/core/src/assertions.ts
+++ packages/core/src/assertions.ts
@@ -5,14 +5,17 @@ export function assert_is_number(value: unknown): asserts value is number {
 	if (typeof value === 'number') {
 		return;
 	}
-	throw new TypeError(`${value} is not a number`);
+	throw new TypeError(`[${value}] is invalid, must be a number`);
 }
 
-export function assert_if_number_is_positive(value: unknown): asserts value is number {
-	if (typeof value === 'number' && value > 0) {
+export function assert_is_valid_size(value: unknown): asserts value is number | 'fill' | 'hug' {
+	if (typeof value === 'number' && value >= 0) {
 		return;
 	}
-	throw new RangeError('value must be a positive number');
+	if (value === 'fill' || value === 'hug') {
+		return;
+	}
+	throw new RangeError(`[${value}] is invalid, must be a non negative number, "fill" or "hug"`);
 }
 
 export function assert_parent_is_component(value: unknown): asserts value is IComponent {
@@ -22,9 +25,9 @@ export function assert_parent_is_component(value: unknown): asserts value is ICo
 	throw new TypeError('parent must be an instance of Component');
 }
 
-export function assert_is_auto_layout(value: unknown): asserts value is 'none' | 'horizontal' | 'vertical' | 'wrap' {
-	if (value === 'none' || value === 'horizontal' || value === 'vertical' || 'wrap') {
+export function assert_is_valid_auto_layout(value: unknown): asserts value is 'none' | 'horizontal' | 'vertical' | 'wrap' {
+	if (value === 'none' || value === 'horizontal' || value === 'vertical' || value === 'wrap') {
 		return;
 	}
-	throw new TypeError('auto_layout must be "none", "horizontal", "vertical" or "wrap"');
+	throw new TypeError(`[${value}] is invalid, must be "none", "horizontal", "vertical" or "wrap`);
 }
