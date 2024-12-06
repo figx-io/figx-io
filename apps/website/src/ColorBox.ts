09761fa2af4fb3c55b947d07aea268dadf52869e
martinrossil-acies <mro@acies.dk>
2024-12-07 00:40:55 +0100

    test: change ColorBox to API

--- apps/website/src/ColorBox.ts
+++ apps/website/src/ColorBox.ts
@@ -4,8 +4,8 @@ export default class ColorBox extends Component {
 	public constructor(color = 'red', size = 300) {
 		super();
 		this.style.background = color;
-		this.style.height = `${size}px`;
-		this.style.width = `${size}px`;
+		this.width = size;
+		this.height = size;
 	}
 }
 customElements.define('color-box', ColorBox);
