9ac34614b105064329e0d1108df313c39b09a9a9
martinrossil-acies <mro@acies.dk>
2024-12-07 00:41:27 +0100

    test: test component sizing

--- apps/website/src/FigxIO.ts
+++ apps/website/src/FigxIO.ts
@@ -6,11 +6,29 @@ export default class FigxIO extends Application {
 		super();
 		// const container = new Component();
 		// this.auto_layout = 'wrap';
-		this.auto_layout = 'horizontal';
-		this.alignment = 'bottom_right';
-		this.add_component(new ColorBox('red', 100));
-		this.add_component(new ColorBox('green', 200));
-		this.add_component(new ColorBox('blue', 300));
+		this.auto_layout = 'vertical';
+		// this.alignment = '';
+		const red = new ColorBox('red', 123);
+		red.alignment = 'center';
+		red.width = 'fill';
+		red.height = 'fill';
+		this.add_component(red);
+		// red.add_component(new ColorBox('green', 150));
+		const green = new ColorBox('green', 200);
+		this.add_component(green);
+		green.width = 'fill';
+		// green.height = 'fill';
+		// const blue = new ColorBox('blue', 300);
+		// blue.width = 'fill';
+		// this.add_component(blue);
+		this.addEventListener('click', () => {
+			if (this.auto_layout === 'horizontal') {
+				this.auto_layout = 'vertical';
+			}
+			else {
+				this.auto_layout = 'horizontal';
+			}
+		});
 		// this.add_component(container);
 	}
 }
