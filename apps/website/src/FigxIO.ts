1dbf951b2dc3d5e24f8c84bf0df8c2a40cab719a
martinrossil-acies <mro@acies.dk>
2024-12-07 23:11:43 +0100

    test: manual layout test

--- apps/website/src/FigxIO.ts
+++ apps/website/src/FigxIO.ts
@@ -4,20 +4,15 @@ import ColorBox from './ColorBox';
 export default class FigxIO extends Application {
 	public constructor() {
 		super();
-		// const container = new Component();
-		// this.auto_layout = 'wrap';
 		this.auto_layout = 'vertical';
-		// this.alignment = '';
-		const red = new ColorBox('red', 123);
-		red.alignment = 'center';
-		red.width = 'fill';
+		const red = new ColorBox('red', 200);
+		// red.width = 'fill';
 		red.height = 'fill';
 		this.add_component(red);
-		// red.add_component(new ColorBox('green', 150));
 		const green = new ColorBox('green', 200);
 		this.add_component(green);
-		green.width = 'fill';
-		// green.height = 'fill';
+		// green.width = 'fill';
+		green.height = 'fill';
 		// const blue = new ColorBox('blue', 300);
 		// blue.width = 'fill';
 		// this.add_component(blue);
