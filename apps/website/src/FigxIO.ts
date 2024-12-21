87b2c841fc5c4648d307396c3c9dd355c725c232
martinrossil-acies <mro@acies.dk>
2024-12-21 14:10:37 +0100

    test: testing min- width and height

--- apps/website/src/FigxIO.ts
+++ apps/website/src/FigxIO.ts
@@ -8,21 +8,18 @@ export default class FigxIO extends Application {
 		const red = new ColorBox('red', 200);
 		// red.width = 'fill';
 		red.height = 'fill';
+		red.min_height = 300;
 		this.add_component(red);
 		const green = new ColorBox('green', 200);
 		this.add_component(green);
 		// green.width = 'fill';
 		green.height = 'fill';
+		green.min_height = 300;
 		// const blue = new ColorBox('blue', 300);
 		// blue.width = 'fill';
 		// this.add_component(blue);
 		this.addEventListener('click', () => {
-			if (this.auto_layout === 'horizontal') {
-				this.auto_layout = 'vertical';
-			}
-			else {
-				this.auto_layout = 'horizontal';
-			}
+			this.auto_layout = this.auto_layout === 'horizontal' ? 'vertical' : 'horizontal';
 		});
 		// this.add_component(container);
 	}
