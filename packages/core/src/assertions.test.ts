5ba999d1e5246e134c42bf71a42406ca7e08407d
martinrossil-acies <mro@acies.dk>
2024-12-29 00:25:41 +0100

    feat: implement is_valid_line_height assertion

--- packages/core/src/assertions.test.ts
+++ packages/core/src/assertions.test.ts
@@ -11,6 +11,7 @@ import {
 	assert_is_valid_alignment,
 	assert_is_valid_auto_layout,
 	assert_is_valid_size,
+	is_valid_line_height,
 } from './assertions';
 import Component from './Component';
 
@@ -195,6 +196,31 @@ describe('assertions', () => {
 		it('when assert_is_from_zero_to_one(2) it should throw a RangeError', () => {
 			expect(() => {
 				assert_is_from_zero_to_one(2);
+			}).toThrow(RangeError);
+		});
+	});
+	describe('is_valid_line_height', () => {
+		it('when is_valid_line_height(1) it should return undefined', () => {
+			const value = is_valid_line_height(1);
+			expect(value).toBeUndefined();
+		});
+		it('when is_valid_line_height("auto") it should return undefined', () => {
+			const value = is_valid_line_height('auto');
+			expect(value).toBeUndefined();
+		});
+		it('when is_valid_line_height(Number.NaN) it should throw a RangeError', () => {
+			expect(() => {
+				is_valid_line_height(Number.NaN);
+			}).toThrow(RangeError);
+		});
+		it('when is_valid_line_height("Hello") it should throw a RangeError', () => {
+			expect(() => {
+				is_valid_line_height('Hello');
+			}).toThrow(RangeError);
+		});
+		it('when is_valid_line_height(0) it should throw a RangeError', () => {
+			expect(() => {
+				is_valid_line_height(0);
 			}).toThrow(RangeError);
 		});
 	});
--- packages/core/src/assertions.ts
+++ packages/core/src/assertions.ts
@@ -84,3 +84,13 @@ export function assert_is_string(value: unknown): asserts value is string {
 	}
 	throw new TypeError(`[${value}] is invalid, must be of type string`);
 }
+
+export function is_valid_line_height(value: unknown): asserts value is number | 'auto' {
+	if (value === 'auto') {
+		return;
+	}
+	if (typeof value === 'number' && value > 0) {
+		return;
+	}
+	throw new RangeError(`[${value}] is invalid, must be more than 0 or "auto"`);
+}
