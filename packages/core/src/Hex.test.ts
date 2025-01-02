fb26de8e2d5296920d847c0bc02f19951a87164d
martinrossil-acies <mro@acies.dk>
2025-01-02 17:28:50 +0100

    feat: add opacity parameter to Hex

--- packages/core/src/Hex.test.ts
+++ packages/core/src/Hex.test.ts
@@ -2,14 +2,30 @@ import { describe, expect, it } from 'vitest';
 import Hex from './Hex';
 
 describe('assert_is_valid_hex', () => {
-	it('when new Hex("#123"), toStyleString() should return "#123"', () => {
-		const hex = new Hex('#123');
-		expect(hex.toStyleString()).toBe('#123');
+	it('when new Hex("#123456"), toStyleString() should return "#123456"', () => {
+		const hex = new Hex('#123456');
+		expect(hex.toStyleString()).toBe('#123456FF');
+	});
+	it('when new Hex("#123456", 50), toStyleString() should return "#12345680"', () => {
+		const hex = new Hex('#123456', 50);
+		expect(hex.toStyleString()).toBe('#12345680');
 	});
 	it('when new Hex("#12345"), it should throw a TypeError', () => {
 		expect(() => {
 			// eslint-disable-next-line no-new
 			new Hex('#12345');
 		}).toThrow(TypeError);
+	});
+	it('when new Hex("#123456", -1), it should throw a RangeError', () => {
+		expect(() => {
+			// eslint-disable-next-line no-new
+			new Hex('#123456', -1);
+		}).toThrow(RangeError);
+	});
+	it('when new Hex("#123456", 101), it should throw a RangeError', () => {
+		expect(() => {
+			// eslint-disable-next-line no-new
+			new Hex('#123456', 101);
+		}).toThrow(RangeError);
 	});
 });
--- packages/core/src/Hex.ts
+++ packages/core/src/Hex.ts
@@ -1,14 +1,22 @@
 import type ISolidColor from './ISolidColor';
+import { assert_is_from_zero_to_hundred } from '@figx-io/assertions/assert_is_from_zero_to_hundred';
 import { assert_is_valid_hex } from '@figx-io/assertions/assert_is_valid_hex';
 
 export default class Hex implements ISolidColor {
 	#hex: `#${string}`;
-	public constructor(hex: `#${string}`) {
+	#opacity: number;
+	public constructor(hex: `#${string}`, opacity = 100) {
 		assert_is_valid_hex(hex);
+		assert_is_from_zero_to_hundred(opacity);
 		this.#hex = hex;
+		this.#opacity = opacity;
 	}
 
 	public toStyleString(): string {
-		return this.#hex;
+		// Scale the value to a range of 0 to 255 (hex "00" to "FF")
+		const scaledValue = Math.round((this.#opacity / 100) * 255);
+		// Convert to a two-digit hexadecimal string
+		const opacityHex = scaledValue.toString(16).padStart(2, '0').toUpperCase();
+		return `${this.#hex}${opacityHex}`;
 	}
 }
