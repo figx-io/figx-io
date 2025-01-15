f0cfe281775073e94f963fb0ebce3a23d3faaf55
martinrossil-acies <mro@acies.dk>
2025-01-15 22:44:25 +0100

    chore: remove unused Rectangle

--- packages/core/src/Rectangle.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, expect, it } from 'vitest';
-import Rectangle from './Rectangle';
-
-describe('rectangle', () => {
-	it('should be able to instantiate Rectangle', () => {
-		const rectangle = new Rectangle();
-		expect(rectangle).toBeInstanceOf(Rectangle);
-	});
-});
